e21c6d1b832ca89404c428c427f5980798e0f31d
Aaron Vega <aaronjv96@yahoo.com>
2022-07-19 17:15:38 -0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+let lon;
+let lat;
+const [temp, setTemp]=useState('_');
+const [location, setLocation]=useState('_');
+const [weather, setWeather]=useState('_');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weatherApp position-absolute top-50 start-50 translate-middle d-flex justify-content-center flex-nowrap">
+      <div className="tempContainer shadow-lg">
+          <div className='location'>{location}</div>
+          <div className="icon"></div>
+          <div className="temp">{temp}°F</div>
+          <div className="weather">{weather}</div>    
+      </div>
     </div>
   );
 }
